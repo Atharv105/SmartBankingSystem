e22728d6dfde52e6ade77c549fd280a046818f52
Atharv105 <atharvdhandale5@gmail.com>
2026-02-01 16:21:42 +0530

    Add User entity

---
+++ Backend/src/main/java/com/smartbanking/entity/User.java
@@ -0,0 +1,61 @@
+package com.smartbanking.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //autoIncrement
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false , unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+//    “JPA applies default column mapping automatically. I only used @Column when I needed specific constraints
+//    like NOT NULL or UNIQUE. For optional fields like contact, default mapping is sufficient.”
+    private String contact;
+
+    //    Default Constructor
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
